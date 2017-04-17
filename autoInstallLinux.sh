dd7301f5120154d504cecc67ad15eb455e3213db
Smoke Jaguar <tanc7@unlv.nevada.edu>
2017-04-17 02:45:38 -0700

    Minor bug fix
    autoInstallLinux should make the proper /root/ArmsCommander/logs/ngrep directory now when autoInstallScript is run.

--- autoInstallLinux.sh
+++ autoInstallLinux.sh
@@ -17,6 +17,7 @@ mkdir /root/ArmsCommander/logs/SQLMap
 mkdir /root/ArmsCommander/logs/OWASP
 mkdir /root/ArmsCommander/logs/Cylon-Raider
 mkdir /root/ArmsCommander/logs/CreditCardInfo
+mkdir /root/ArmsCommander/logs/ngrep
 echo Copying main installation into /root/ArmsCommander
 cp -r ./ /root/ArmsCommander #copies contents to the new directory
 echo Copying launcher icon to Kali Desktop
@@ -24,10 +25,12 @@ cp /root/ArmsCommander/Launch\ Arms\ Commander.desktop /root/Desktop # move laun
 echo Copying main launcher file to /usr/local/bin
 chmod 777 ./ArmsCommander.py #makes the launcher executable
 cp -r ./ArmsCommander.py /usr/local/bin #copies the main launcher into the /usr/local/bin directory, allowing someone to start AC by typing "ArmsCommander.py" in terminal
+echo Installing python modules via pip
+pip install -r requirements.txt # installs all required python modules
 echo Main Installation Completed
 echo Open a terminal and type "ArmsCommander.py" to run AC, or click the Desktop Launcher
-echo To use the Pupy alternative RAT interface (Python-based, Similar to Meterpreter)
-echo You must read the installation instructions in the READM.md
+echo To use the Pupy alternative RAT interface Python-based, Similar to Meterpreter
+echo You must read the installation instructions in the README.md
 echo or
 echo Take a look at this How-To-Video
 echo https://github.com/tanc7/Arms-Commander/blob/master/How-To-Videos/How-To-Install-Pupy.webm
