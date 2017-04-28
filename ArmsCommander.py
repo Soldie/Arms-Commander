79d63a175d074db3da0955d5bccdb864ec03c883
Smoke Jaguar <tanc7@unlv.nevada.edu>
2017-04-28 13:28:42 -0700

    The Second Prune (Not Required Update)
    For usability purposes i performed a second prune to reorganize the Remote Exploits Menu. It was starting to get disorganized and ugly.
    
    There are only three categories now
    
    >1. Payload Generators/Listeners
    
    >2. Toolkits
    
    >3. Other Remote Exploitation Stuff
    
    There is NO change in usability. But from now on, this is what AC will look like for future integration with other software. Easier on your eyes.

--- ArmsCommander.py
+++ ArmsCommander.py
@@ -87,7 +87,7 @@ def output_data():
         print colored('All log files imported to /root/ArmsCommander/logs','red','on_white')
         output_data()
     else:
-        print 'You have entered a invalid option'
+        print colored('You have entered a invalid option','red')
         output_data()
 
     return
@@ -133,7 +133,7 @@ def one_recon_and_vuln_scan():
         os.system('clear')
         output_data()
     else:
-        print 'You have entered a invalid option'
+        print colored('You have entered a invalid option','red')
         one_recon_and_vuln_scan()
     return
 
@@ -277,7 +277,7 @@ def two_net_defense_traffic_monitor():
         os.system('clear')
         IDS_Flooder()
     else:
-        print 'You have entered a invalid option'
+        print colored('You have entered a invalid option','red')
         two_net_defense_traffic_monitor()
 
     return
@@ -339,7 +339,7 @@ def VT_Checker():
     elif opt_Choice == "0":
         main()
     else:
-        print 'You have entered a invalid option'
+        print colored('You have entered a invalid option','red')
         VT_Checker()
     main()
     return
@@ -353,20 +353,12 @@ def stitch_menu():
     os.system('python /root/ArmsCommander/remoteexploits/Stitch_Menu.py')
     return
 
-def three_remote_exploits():
+def payload_generators_listeners():
     opt_List = [
         '\n\t#0. Return to Main Menu',
-        '#1. Metasploit Framework, with a Manual-Start Script to overcome any database init issues',
-        '#2. Armitage',
-        '#3. Easy-Peasey, msfvenom payload generator',
-        '#4. Veil-Evasion, changes signature of msfvenom type payloads',
-        '#5. Social Engineers Toolkit (SET), a expansive toolkit by itself that covers spearphishing attacks to SMS spoofing to Arduino-attacks',
-        '#6. Pupy, Cross-Platform, Pythonic version of a Remote Access Trojan (RAT) with abilities that are comparable/superior to Meterpreter shells, with added benefits of being rarer and therefore harder to detect, and supports powershell injection',
-        '#7. Dont Patch Me Bro :(, the easy-mode inject.bin generator for Hak5 USB Rubber Duckies',
-        '#8. Virus-Total Checker, safely check whether or not your custom payload is detectable without alerting Virus-Total and AV developers',
-        '#9. Embed a custom made payload into a PDF file',
-        '#10. Stitch, another Cross-Platform, Pythonic RAT'
-
+        '#1. EASY-PEASEY, the EZ-Mode Interactive MSFVenom Payload Generation Menu',
+        '#2. PUPY, the alternative Python Remote Access Trojan (RAT)',
+        '#3. STITCH, another alternative Python RAT that looks like the easiest to use'
     ]
 
     print ("\n\t".join(opt_List))
@@ -375,47 +367,190 @@ def three_remote_exploits():
 
     if opt_Choice == "0":
         os.system('clear')
+        three_remote_exploits_redesigned()
+    elif opt_Choice == "1":
+        os.system('clear')
+        easy_peasey()
+        three_remote_exploits_redesigned()
+    elif opt_Choice == "2":
+        os.system('clear')
+        pupy_menu()
+        three_remote_exploits_redesigned()
+    elif opt_Choice == "3":
+        os.system('clear')
+        stitch_menu()
+        three_remote_exploits_redesigned()
+    else:
+        print colored('You have entered a invalid option','red')
+        payload_generators_listeners()
+    return
+
+def remote_exploits_toolkits():
+    opt_List = [
+        '\n\t#0. Return to Main Menu',
+        '#1. VEIL-EVASION, change the signature of a Metasploit payload to evade antivirus (the msfvenom encoder hides you from Intrusion Detection Systems ONLY)',
+        '#2. METASPLOIT FRAMEWORK, this is a CUSTOM MANUAL-START SCRIPT to get past those pesky database initialization errors',
+        '#3. ARMITAGE, the free version of what would later be Cobalt Strike. Still useful in pentests.',
+        '#4. SOCIAL ENGINEERS TOOLKIT, comes with preloaded scripts and a easy-to-use push-button style menu for hacking techniques such as spearphishing'
+    ]
+    print ("\n\t".join(opt_List))
+    opt_Choice = str(raw_input("Enter a OPTION: "))
+
+    if opt_Choice == "0":
+        os.system('clear')
+        three_remote_exploits_redesigned()
+    elif opt_Choice == "1":
+        os.system('clear')
+        # placeholder for specific function
+        print colored('[+] Starting Veil Evasion','green')
+        veil_evasion()
+        three_remote_exploits_redesigned()
+    elif opt_Choice == "2":
+        os.system('clear')
+        # placeholder for specific function
+        print colored('[+] Starting Metasploit Manual-Start Mode','green')
+        metasploit()
+        three_remote_exploits_redesigned()
+    elif opt_Choice == "3":
+        os.system('clear')
+        # placeholder for specific function
+        print colored('[+] Starting Armitage','green')
+        armitage()
+        three_remote_exploits_redesigned()
+    elif opt_Choice == "4":
+        os.system('clear')
+        # placeholder for specific function
+        print colored('[+] Starting Social Engineers Toolkit','green')
+        social_engineers_toolkit()
+        three_remote_exploits_redesigned()
+    else:
+        print colored('You have entered a invalid option','red')
+        remote_exploits_toolkits()
+        # placeholder for current menu
+    return
+
+def remote_exploits_other():
+    opt_List = [
+        '\n\t#0. Return to Main Menu',
+        '#1. DONT PATCH ME BRO, the easy-mode menu to generate inject.bin files for DuckEncoder and DuckyScript Languages on USB Rubber Duckies',
+        '#2. VIRUS-TOTAL SAFE-CHECKER (VT-Notify of Veil Evasion), compare just the hash of a newly generated payload without submitting it to VT to avoid being added to antivirus databases'
+    ]
+    print ("\n\t".join(opt_List))
+    opt_Choice = str(raw_input("Enter a OPTION: "))
+
+    if opt_Choice == "0":
+        os.system('clear')
+        three_remote_exploits_redesigned()
+    elif opt_Choice == "1":
+        os.system('clear')
+        print colored('[+] Starting Dont Patch Me Bro','green')
+        # placeholder for specific function
+        DPMB()
+        three_remote_exploits_redesigned()
+    elif opt_Choice == "2":
+        os.system('clear')
+        print colored('[+] Starting Virus-Total Safe-Checker','green')
+        # placeholder for specific function
+        VT_Checker()
+        three_remote_exploits_redesigned()
+    else:
+        print colored('You have entered a invalid option','red')
+        # placeholder for current menu
+    return
+
+def three_remote_exploits_redesigned(): # Tentative project to reduce bloat
+    opt_List = [
+        '\n\t#0. Return to Main Menu',
+        '#1. PAYLOAD GENERATORS & LISTENERS, Metasploit Msfvenom, and open source alternatives RATs like Pupy and Stitch',
+        '#2. TOOLKITS, Social Engineers Toolkit',
+        '#3. OTHER, Does not fit in any other category, USB Rubber Ducky Encoders, stuff like Virus-Total Safe-Checker'
+    ]
+
+    print ("\n\t".join(opt_List))
+    opt_Choice = str(raw_input("Enter a OPTION: "))
+
+    if opt_Choice == "0":
+        os.system('clear')
         main()
     elif opt_Choice == "1":
         os.system('clear')
-        metasploit()
-        # MT_host_recon()
+        payload_generators_listeners()
+        three_remote_exploits_redesigned()
     elif opt_Choice == "2":
         os.system('clear')
-        armitage()
-        # CornHarvester()
+        remote_exploits_toolkits()
+        three_remote_exploits_redesigned()
     elif opt_Choice == "3":
         os.system('clear')
-        easy_peasey()
-        # NMap_Auto()
-    elif opt_Choice == "4":
-        os.system('clear')
-        veil_evasion()
-        # NMap_Custom()
-    elif opt_Choice == "5":
-        os.system('clear')
-        social_engineers_toolkit()
-        # SQLMap()
-    elif opt_Choice == "6":
-        os.system('clear')
-        pupy_menu()
-        # OWASP_zaproxy()
-    elif opt_Choice == "7":
-        os.system('clear')
-        DPMB()
-    elif opt_Choice == "8":
-        os.system('clear')
-        VT_Checker()
-        # burpsuite()
-    elif opt_Choice == "9":
-        os.system('clear')
-        embed_payload_pdf()
-    elif opt_Choice == "10":
-        os.system('clear')
-        stitch_menu()
+        remote_exploits_other()
+        three_remote_exploits_redesigned()
     else:
-        print 'You have entered a invalid option'
-        three_remote_exploits()
+        print colored('You have entered a invalid option','red')
+        three_remote_exploits_redesigned()
+    return
+# def three_remote_exploits():
+#     opt_List = [
+#         '\n\t#0. Return to Main Menu',
+#         '#1. Metasploit Framework, with a Manual-Start Script to overcome any database init issues',
+#         '#2. Armitage',
+#         '#3. Easy-Peasey, msfvenom payload generator',
+#         '#4. Veil-Evasion, changes signature of msfvenom type payloads',
+#         '#5. Social Engineers Toolkit (SET), a expansive toolkit by itself that covers spearphishing attacks to SMS spoofing to Arduino-attacks',
+#         '#6. Pupy, Cross-Platform, Pythonic version of a Remote Access Trojan (RAT) with abilities that are comparable/superior to Meterpreter shells, with added benefits of being rarer and therefore harder to detect, and supports powershell injection',
+#         '#7. Dont Patch Me Bro :(, the easy-mode inject.bin generator for Hak5 USB Rubber Duckies',
+#         '#8. Virus-Total Checker, safely check whether or not your custom payload is detectable without alerting Virus-Total and AV developers',
+#         '#9. Embed a custom made payload into a PDF file',
+#         '#10. Stitch, another Cross-Platform, Pythonic RAT'
+#
+#     ]
+#
+#     print ("\n\t".join(opt_List))
+#
+#     opt_Choice = str(raw_input("Enter a OPTION: "))
+#
+#     if opt_Choice == "0":
+#         os.system('clear')
+#         main()
+#     elif opt_Choice == "1":
+#         os.system('clear')
+#         metasploit()
+#         # MT_host_recon()
+#     elif opt_Choice == "2":
+#         os.system('clear')
+#         armitage()
+#         # CornHarvester()
+#     elif opt_Choice == "3":
+#         os.system('clear')
+#         easy_peasey()
+#         # NMap_Auto()
+#     elif opt_Choice == "4":
+#         os.system('clear')
+#         veil_evasion()
+#         # NMap_Custom()
+#     elif opt_Choice == "5":
+#         os.system('clear')
+#         social_engineers_toolkit()
+#         # SQLMap()
+#     elif opt_Choice == "6":
+#         os.system('clear')
+#         pupy_menu()
+#         # OWASP_zaproxy()
+#     elif opt_Choice == "7":
+#         os.system('clear')
+#         DPMB()
+#     elif opt_Choice == "8":
+#         os.system('clear')
+#         VT_Checker()
+#         # burpsuite()
+#     elif opt_Choice == "9":
+#         os.system('clear')
+#         embed_payload_pdf()
+#     elif opt_Choice == "10":
+#         os.system('clear')
+#         stitch_menu()
+#     else:
+#         print colored('You have entered a invalid option','red')
+#         three_remote_exploits()
 
 def APK_Malware_Injection():
     os.system('python /root/ArmsCommander/mobiledevices/APKmalwareInjector.py')
@@ -445,7 +580,7 @@ def four_mobile_dev_hacking():
         DIAMONDSHARK()
         # MT_host_recon()
     else:
-        print 'You have entered a invalid option'
+        print colored('You have entered a invalid option','red')
         four_mobile_dev_hacking()
     return
 
@@ -532,7 +667,7 @@ def five_wireless_attacks():
         hidden_network_decloaker()
         # SQLMap()
     else:
-        print 'You have entered a invalid option'
+        print colored('You have entered a invalid option','red')
         five_wireless_attacks()
 
 def aircrack():
@@ -556,7 +691,7 @@ def six_password_attacks():
         aircrack()
         # MT_host_recon()
     else:
-        print 'You have entered a invalid option'
+        print colored('You have entered a invalid option','red')
         six_password_attacks()
     return
 
@@ -592,7 +727,7 @@ def seven_book_learning():
     elif Exception:
         error_handling()
     else:
-        print 'You have entered a invalid option'
+        print colored('You have entered a invalid option','red')
         seven_book_learning()
     return
 
@@ -620,7 +755,7 @@ def main():
         two_net_defense_traffic_monitor()
     elif opt_Choice == "3":
         os.system('clear')
-        three_remote_exploits()
+        three_remote_exploits_redesigned()
     elif opt_Choice == "4":
         os.system('clear')
         four_mobile_dev_hacking()
@@ -634,7 +769,7 @@ def main():
         os.system('clear')
         seven_book_learning()
     else:
-        print 'You have entered a invalid option'
+        print colored('You have entered a invalid option','red')
         error_handling()
         main()
     main()
