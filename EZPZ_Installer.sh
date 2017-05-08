f6207877ebc28cf9f4344d78c771252869ac94e7
tanc7 <tanc7@unlv.nevada.edu>
2017-05-08 06:49:11 -0700

    cleanup update

--- EZPZ_Installer.sh
+++
@@ -1,4 +0,0 @@
-mkdir /root/ArmsCommander
-cp -r ./ /root/ArmsCommander
-chmod 777 EZPZ.py
-cp -r EZPZ /usr/local/bin
--- pupy_server_startup.sh
+++
@@ -1,2 +0,0 @@
-cd /root/pupy/pupy
-sudo python /root/pupy/pupy/pupysh.py -t ssl -p 443
